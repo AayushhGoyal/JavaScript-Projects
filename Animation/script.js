3a2ae3cacee1059947eca0ea5e3f771fe5555103
Ayush Goyal <agayush088@gmail.com>
2024-12-01 16:33:34 +0530

    Complete Smooth Scroll animation

---
+++ Animation/script.js
@@ -0,0 +1,8 @@
+function smoothScroll(target, duration){
+    var target = document.querySelector(target);
+    var targetPosition = target.getBoundingClientRect();
+    console.log(target);
+}
+
+
+smoothScroll('.section2', 1000)
